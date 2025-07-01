de3f96a21ff89f7c3c4945739f861621b41c3e1d
e-ji1107 <diddmswl1107@gmail.com>
2025-07-01 13:59:51 +0900

    0701_SpringBoot

--- day07/backboard/src/main/java/com/pknu/backboard/entity/Board.java
+++ day07/backboard/src/main/java/com/pknu/backboard/entity/Board.java
@@ -1,39 +1,42 @@
 package com.pknu.backboard.entity;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.OneToMany;
+import lombok.*;
 
 @Getter
-@Setter // 롬복으로 Getter/Setter 자동화
-@Entity // JPA 테이블 매핑 선언
+@Setter // Lombok을 사용하여 Getter와 Setter 자동 생성
+@Entity // JPA 엔티티로 지정
 public class Board {
 
-    @Id // PK
+    @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Long bno; // Board 테이블의 PK, bno
+    private Long bno;   // Board 테이블의 PK, bno
 
-    // @Column(name="subject", length = 250)
     @Column(length = 250)
-    private String title; // 게시판 제목
+    private String title;   // 게시판 제목
 
-    @Column(length = 8000)
+    @Column(columnDefinition = "CLOB")
     private String content; // 게시글 내용
 
     @CreatedDate
-    @Column(updatable = false)  // 한번 작성 후 수정하지 않음
-    private LocalDateTime createDate; // 게시글 작성일
+    @Column(updatable = false) // 한번 작성 후 수정하지 않음
+    private LocalDateTime creatDate;    // 게시글 작성일
 
     @LastModifiedDate
-    private LocalDateTime modifyDate; // 게시글 수정일
+    private LocalDateTime modifyDate;   // 게시글 수정일
 
+    @OneToMany(mappedBy = "board", cascade = CascadeType.REMOVE) 
+    private List<Reply> replyList; // 댓글 목록
 }
--- day07/backboard/src/test/java/com/pknu/backboard/BackboardApplicationTests.java
+++ day07/backboard/src/test/java/com/pknu/backboard/BackboardApplicationTests.java
@@ -10,13 +10,14 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListeners;
 
 import com.pknu.backboard.entity.Board;
 import com.pknu.backboard.repository.BoardRepository;
 
 @SpringBootTest
 class BackboardApplicationTests {
+
+	// TDD
 
 	@Autowired
 	private BoardRepository boardRepository;
@@ -25,26 +26,27 @@ class BackboardApplicationTests {
 	void contextLoads() {
 	}
 
-	@Test  // INSERT 테스트
+	@Test
 	void testInsertJpa() {
 		Board board1 = new Board();
-		board1.setTitle("첫번째 게시글입니다.");  // @Setter에서 자동 생성해주는 메서드
+		board1.setTitle("첫번째 게시글입니다."); // @Setter에서 자동 생성해주는 메서드
 		board1.setContent("내용은 없습니다.");
-		board1.setCreateDate(LocalDateTime.now());
+		board1.setCreatDate(LocalDateTime.now());
 
 		Board board2 = new Board();
 		board2.setTitle("두번째 게시글입니다.");
 		board2.setContent("내용은 없다니까요.");
-		board2.setCreateDate(LocalDateTime.now());
+		board2.setCreatDate(LocalDateTime.now());
 
-		this.boardRepository.save(board1); // INSERT 실행
-		this.boardRepository.save(board2); // INSERT 실행
+		this.boardRepository.save(board1); // INSERT 진행
+		this.boardRepository.save(board2); // INSERT 진행
+
 	}
 
 	@Test // SELECT * 테스트
 	void testSelectJpa() {
 		List<Board> all = this.boardRepository.findAll();
-		// 테스트시 내가 기대하는 값과 현재 값이 같은지 확인메서드
+		// 테스트시 내가 기대하는 값과 현재 값이 같은지 확인 메서드
 		assertEquals(2, all.size());
 
 		Board board = all.get(0);
@@ -53,7 +55,7 @@ class BackboardApplicationTests {
 
 	@Test // SELECT FROM WHERE 테스트
 	void testSelectOneJpa() {
-		// Optional -> Null을 허용
+		// Optrional -> Null을 허용
 		Optional<Board> opBoard = this.boardRepository.findById(2L);
 		if (opBoard.isPresent()) {
 			Board board = opBoard.get();
@@ -65,14 +67,15 @@ class BackboardApplicationTests {
 	void testSelectByTitle() {
 		Board board = this.boardRepository.findByTitle("두번째 게시글입니다.");
 		assertEquals(2L, board.getBno());
+
 	}
 
-	@Test // SELECT FROM LIKE
+	@Test
 	void testSelectByTitleLike() {
 		List<Board> boards = this.boardRepository.findByTitleLike("%게시글%");
 		assertEquals(4, boards.size());
 
-		Board board = boards.get(0); // 첫번째 게시글
+		Board board = boards.get(0);
 		assertEquals("첫번째 게시글입니다.", board.getTitle());
 	}
 
@@ -83,7 +86,18 @@ class BackboardApplicationTests {
 		assertTrue(opBoard.isPresent());
 
 		Board board = opBoard.get();
-		this.boardRepository.delete(board);
-		assertEquals(3, boardRepository.count());  // 한건 지워서 3건 남음
+		this.boardRepository.delete(board); // 삭제
+		assertEquals(3, boardRepository.count());
+	}
+
+	@Test // 수정
+	void testUpdateOne() {
+		Optional<Board> opBoard = this.boardRepository.findById(1L);
+		assertTrue(opBoard.isPresent());
+
+		Board board = opBoard.get();
+		board.setContent("내용이 테스트에서 변경되었습니다!!!");
+
+		this.boardRepository.save(board); // 수정은 save로 가능
 	}
 }
